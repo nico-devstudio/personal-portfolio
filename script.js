c9a56f7a5e6a3734479814fd00869ebf5998d5d1
Nico Angelo  Garilva <nicogarilva@gmail.com>
2026-06-22 22:28:11 +0800

    creating nav__link styles and JS behavior

---
+++ script.js
@@ -0,0 +1,28 @@
+const navMenu = document.getElementById('nav-menu'),
+    navToggle = document.getElementById('nav-toggle'),
+    navClose = document.getElementById('nav-close')
+
+// Menu Show
+
+if (navToggle) {
+    navToggle.addEventListener('click', () => {
+        navMenu.classList.add('show-menu')
+    })
+}
+
+// Menu Hidden
+if (navClose) {
+    navClose.addEventListener('click', () => {
+        navMenu.classList.remove('show-menu')
+    })
+}
+
+// Remove Menu Mobile
+
+const navLink = document.querySelectorAll('.nav__link')
+
+const linkAction = () => {
+    navMenu.classList.remove('show-menu')
+}
+
+navLink.forEach(n => n.addEventListener('click', linkAction))
